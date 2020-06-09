5e974f3585afbb3783dacf0a537ed6cdc04d9ff0
Neo Thokoa <nthokoa16@gmail.com>
2020-06-09 18:37:28 +0200

    Remove Avatar

--- src/client/app/app.js
+++ src/client/app/app.js
@@ -1,3 +1,5 @@
-angular.module('SecretSocietyChat', ['ngRoute', "ngGravatar", "btford.socket-io"]).config(function(GravatarProvider){
-  GravatarProvider.setSize(100);
-});
+// angular.module('SecretSocietyChat', ['ngRoute', "ngGravatar", "btford.socket-io"]).config(function(GravatarProvider){
+//   GravatarProvider.setSize(100);
+// });
+
+angular.module('SecretSocietyChat', ['ngRoute', "btford.socket-io"]);
--- src/client/app/directives/avatar/avatar.js
+++ src/client/app/directives/avatar/avatar.js
@@ -1,27 +1,27 @@
-(function() {
-      'use strict';
+// (function() {
+//       'use strict';
 
-      angular.module('SecretSocietyChat').directive("avatar",['Gravatar', 'emailFilter', function(Gravatar, emailFilter){
-        return {
-          replace: true,
-          restrict: "E",
-          template: "<img ng-src='{{imageURL()}}' class=\"avatar\">",
-          scope: {user: "="},
-          link: function(scope){
-            scope.imageURL = function(){
-                  var username = scope.user.name;
-                  // Check if username is email
-                  if(emailFilter(username)) {
-                        return Gravatar(username);
-                  } else {
-                        // Generate Identicon for username
-                        var hash = CryptoJS.MD5(username).toString(CryptoJS.enc.Base64);
-                        var data = new Identicon(hash, 250).toString();
-                        return 'data:image/png;base64,' + data;
-                  }
-            };
-          }
-        };
-      }]);
+//       angular.module('SecretSocietyChat').directive("avatar",['Gravatar', 'emailFilter', function(Gravatar, emailFilter){
+//         return {
+//           replace: true,
+//           restrict: "E",
+//           template: "<img ng-src='{{imageURL()}}' class=\"avatar\">",
+//           scope: {user: "="},
+//           link: function(scope){
+//             scope.imageURL = function(){
+//                   var username = scope.user.name;
+//                   // Check if username is email
+//                   if(emailFilter(username)) {
+//                         return Gravatar(username);
+//                   } else {
+//                         // Generate Identicon for username
+//                         var hash = CryptoJS.MD5(username).toString(CryptoJS.enc.Base64);
+//                         var data = new Identicon(hash, 250).toString();
+//                         return 'data:image/png;base64,' + data;
+//                   }
+//             };
+//           }
+//         };
+//       }]);
 
-})();
+// })();
